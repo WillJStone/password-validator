80324c4e66265331f3d9fb0df72fa0b56e2eb46d
Will Stone <will.stone143@gmail.com>
2018-02-07 14:09:57 -0400

    stage 2 complete

--- app/src/test/java/ca/csci3130/cs3130_a2/ExampleUnitTest.java
+++ app/src/test/java/ca/csci3130/cs3130_a2/ExampleUnitTest.java
@@ -14,13 +14,25 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
+    @Test // Reject password if it is "password". Will pass lenCheck
     public void passwdCheck() throws Exception {
-        assertEquals(1,Validator.validate("Password"));
+        assertEquals(1, Validator.validate("password"));
     }
-    @Test
+    @Test // password at least 8 chars. Will pass passwdCheck and lenCheck
     public void lenCheck() throws Exception {
-        assertEquals(2,Validator.validate("mypassword"));
-
+        assertEquals(2, Validator.validate("mypassword"));
+    }
+    @Test // Password needs a number. Will pass passwdCheck, lenCheck, and numCheck,
+    public void numCheck() throws Exception {
+        assertEquals(3, Validator.validate("mypassword1"));
+    }
+    @Test //make sure password has one upper case letter. Will pass passwdCheck, lenCheck, numCheck,
+          // and caseCheck
+    public void caseCheck() throws Exception {
+        assertEquals(4, Validator.validate("myPassword1"));
+    }
+    @Test // Make sure password has special character. Will pass all 5 tests
+    public void charCheck() throws Exception {
+        assertEquals(5, Validator.validate("myPassword^1"));
     }
 }
